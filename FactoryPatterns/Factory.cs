81e88dc5d5896da6f056fe62edda38b57b22dff3
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-21 08:40:55 -0700

    First commit

---
+++ FactoryPatterns/Factory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatterns
+{
+    public static class Factory
+    {
+        public static IVehical GetVehical(string tires)
+        {
+            if (tires == "2")
+            {
+                var x = new Motorcycle();
+                return x;
+            }
+            else
+            {
+                var y = new Truck();
+                return y;
+            }
+        }
+    }
+}
---
+++ FactoryPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FactoryPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("How many wheels are on your vehical?");
+           var answer = Console.ReadLine();
+           var x = Factory.GetVehical(answer);
+            x.Drive();
+
+        }   
+    }
+}
